227e83f882b797478384c2b553986e385b113c0f
Stephen Leake <stephen_leake@stephe-leake.org>
2022-07-10 16:53:21 -0700

    Release beta testing version 4.0.beta
    * NEWS: Beta version.
    
    * wisi.el: Beta version.

--- wisi.el
+++ wisi.el
@@ -7,7 +7,7 @@
 ;; Keywords: parser
 ;;  indentation
 ;;  navigation
-;; Version: 3.1.8
+;; Version: 4.0.beta
 ;; package-requires: ((emacs "25.3") (seq "2.20"))
 ;; URL: http://stephe-leake.org/ada/wisitoken.html
 ;;
